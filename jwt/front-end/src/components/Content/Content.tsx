e60bc685abfa27de457bcd7a44bf048975be5adc
tothricsaj <tothricsaj@gmail.com>
2022-10-05 12:38:15 +0200

    feat: FE - fetch content

--- jwt/front-end/src/components/Content/Content.tsx
+++ jwt/front-end/src/components/Content/Content.tsx
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
 const Content = (): JSX.Element => {
+	const [content, setContent] = useState<string | null>('Pleas login to see content!');
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const res = await fetch('http://localhost:5000/content');
+				const fetchedContent = await res.json();
+
+				setContent(fetchedContent.content);
+			} catch(e) {
+				console.log(e);
+			}
+		}
+
+		fetchData();
+	}, [content]);
+
 	return (
 		<div>
-			content
+			{content}
 		</div>
 	);
 }
