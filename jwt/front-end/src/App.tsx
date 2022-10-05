262b7f280899d32c892843f17bd32002f7769c29
tothricsaj <tothricsaj@gmail.com>
2022-10-05 11:53:02 +0200

    chore: removed dead code

--- jwt/front-end/src/App.tsx
+++ jwt/front-end/src/App.tsx
@@ -25,79 +25,33 @@ function App() {
   const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault();
 
-    const res = await fetch('http://localhost:5000/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(inputValues)
-    });
+    try {
+      const res = await fetch('http://localhost:5000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(inputValues)
+      });
 
-    const data = await res.json();
+      const data = await res.json();
 
-    if(res.status === 401) {
-      setMessage(data.message);
+      if(res.status === 401) {
+        setMessage(data.message);
+      }
+
+      if(res.status === 200) {
+        setIsAuth(true);
+        setToken(data.token);
+        setUserName(data.userName);
+        setMessage(data.message);
+
+        localStorage.setItem('token', data.token);
+      }
+    } catch(e) {
+      console.log(e);
     }
-
-    if(res.status === 200) {
-      setIsAuth(true);
-      setToken(data.token);
-      setUserName(data.userName);
-      setMessage(data.message);
-
-      localStorage.setItem('token', data.token);
-    }
-    
-    
-    // .then(res => {
-    //   console.log('res.json -> ', res.json);
-      
-    //   return res.json();
-    // })
-    // .then((data: any) => {
-
-    //   const {fetchedData, statusCode} = data;
-      
-    //   console.log('data ->  ', data);
-    //   if(statusCode === 401) {
-    //     console.log('inside');
-        
-    //     setMessage(fetchedData.message);
-
-        
-    //   }
-
-    //   if(statusCode === 200) {
-    //     setIsAuth(true);
-    //     setToken(fetchedData.token);
-    //     setUserName(fetchedData.userName);
-
-    //     localStorage.setItem('token', data.token);
-    //   }
-    // })
-    // .catch(err => console.log(err)
-    // )
-    // .finally(() => {
-    //     console.log('message -> ', message);
-    // })
   }
-
-  // useEffect(() => {
-  //   fetch('http://localhost:5000/content')
-  //     .then(res => {
-  //       return res.json();
-  //     })
-  //     .then(data => {
-  //       console.log('data', data);
-        
-  //       setContent(data.content);
-  //     })
-  //     .catch(e => console.log(e)
-  //     )
-  //     .finally(() => {
-  //       setFetchError('Content not Fetched')
-  //     })
-  // });
 
   return (
     <div className="App">
